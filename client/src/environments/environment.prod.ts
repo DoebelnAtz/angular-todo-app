7276f5edb3dc03993ed34d79e798452fa1612a5f
doebelnatz <axel.ej.adlercreutz@gmail.com>
2021-05-25 22:48:21 +0300

    firebase hosting for client

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-import { firebaseConfig } from './firebase'
-
+import { firebaseConfig } from './firebase';
 
 export const environment = {
-  production: true,
-	firebase: firebaseConfig
+	production: true,
+	firebase: firebaseConfig,
+	url: 'http://localhost:3000/api',
 };
